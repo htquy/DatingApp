960d60a27fc59485e822d336ef82c0f004917d95
htquy <hoangquydz0212@gmail.com>
2023-02-26 17:32:51 +0700

    end of section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',//kich hoat chay code trong mau "app-root"
+  templateUrl: './app.component.html',//duong dan URL tuyet doi
+  styleUrls: ['./app.component.css']//duong dan cung cap css
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'The Dating App';
+  users:any;
+  constructor(private http:HttpClient){}
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get('http://localhost:5193/api/users').subscribe(response=>{
+      this.users=response;
+    },error =>{console.log(error)}
+    );//log ra ket qua du lieu tu API qua phuong thuc subscribe
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ],// cai dat cac component,pipe,dirctive cho chuong trinh
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],// tap hop cac dodule co kha nang cung cap cho chuong trinh
+  providers: [],//cung cap module(component) de su dung cho chuong trinh khac
   bootstrap: [AppComponent]
 })
 export class AppModule { }
