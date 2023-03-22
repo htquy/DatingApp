4c54de8652dc8b1b7f46c45d1d903452eb9c98fd
htquy <hoangquydz0212@gmail.com>
2023-03-22 15:56:07 +0700

    end of 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles(){
+            CreateMap<AppUser,MemberDto>().ForMember(dest=>dest.PhotoUrl,opt=>opt.MapFrom(src=>src.Photos.FirstOrDefault(x=>x.IsMain).Url))
+            .ForMember(dest=>dest.Age,opt=>opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
+            //anh xa cac thuoc tinh cua AppUser cho MemberDto va thuoc tinh PhotoUrl duoc anh xa tu Photos.Url
+            CreateMap<Photo,PhotoDto>();
+        }
+    }
+}
