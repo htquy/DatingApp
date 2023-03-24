e28abb42e50ea939bc5ed5eed6afc163f16ccac0
htquy <hoangquydz0212@gmail.com>
2023-03-24 15:34:02 +0700

    end of 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -12,29 +12,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{   [Authorize]
+{
+    [Authorize]
     public class UsersController : BaseApiController
     {
+        private readonly DataContext _context;
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
 
-        public UsersController(IUserRepository userRepository,IMapper mapper)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
             _mapper = mapper;
-            _userRepository=userRepository;
+            _userRepository = userRepository;
         }
+        [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {   
-            var users=await _userRepository.GetMembersAsync(); 
+
+        public async  Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users =await _userRepository.GetMembersAsync();
 
             return Ok(users);
         }
         [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username)
-        {  return await _userRepository.GetMemberAsync(username);
-            
-        }
 
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
+        }
     }
 }
